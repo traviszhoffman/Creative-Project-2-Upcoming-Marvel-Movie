b27e78457d6be3645a2bedc118026a5af320f987
Travis Hoffman <traviszhoffman@gmail.com>
2022-04-18 21:01:01 -0600

    made some changes to button and added more data from api

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.getElementById("buttonClick").addEventListener("click", function (event
             let results = "<div class='card center'>";
             results += '<h2>Releases in ' + json.days_until + " day";
             if(json.days_until > 1) results +='\'s';
-            results += "</h2>";
+            results += " on " +json.release_date + "</h2>";
             results += '<h1>' + json.title + "</h1>"
             results += '<img src="' + json.poster_url + '">';
             results += '<h3>' + "Next up afterward is " + json.following_production.title + '</h3>';
